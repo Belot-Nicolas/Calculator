ff8fd11b791ab9b7cc51acedfb487618f3c5b9fb
belot nicolas <nicolas-belot_student2021@wilder.school>
2022-06-21 10:38:57 +0200

    calculator

---
+++ calculator.js
@@ -0,0 +1,37 @@
+const touches = [...document.querySelectorAll('.buttonCalculator')];
+const listeKeycode = touches.map(touche => touche.dataset.key);
+const screen = document.querySelector('.screen');
+
+document.addEventListener('keydown', (e) => {
+    const value = e.keyCode.toString();
+    calculer(value)
+
+})
+
+document.addEventListener('click', (e) => {
+    const value = e.target.dataset.key;
+    calculer(value)
+
+})
+
+const calculer = (value) => {
+    if (listeKeycode.includes(value)) {
+        switch (value) {
+            case '8':
+                screen.textContent = "";
+                break;
+            case '13':
+                const calcul = eval(screen.textContent);
+                screen.textContent = calcul;
+                break;
+            default:
+                const indexKeycode = listeKeycode.indexOf(value);
+                const touche = touches[indexKeycode];
+                screen.textContent += touche.innerHTML;
+        }
+    }
+}
+
+window.addEventListener('error', (e) => {
+    alert('Une erreur est survenue dans votre calcul : ' + e.message)
+})
